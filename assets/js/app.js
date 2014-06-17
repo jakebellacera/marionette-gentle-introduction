18fde04296914e5e0f1e65751bfa16519734d94e
Jake Bellacera <jakeb@eleveninc.com>
2014-06-17 16:28:45 -0700

    Adding an app router

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,22 @@ ContactManager.addRegions({
   mainRegion: "#main-region"
 });
 
-ContactManager.on("initialize:after", function () {
-  ContactManager.ContactsApp.List.Controller.listContacts();
+ContactManager.navigate(function (route, options) {
+  options || (options = {});
+  Backbone.history.navigate(route, options);
 });
 
+ContactManager.getCurrentRoute = function () {
+  return Backbone.history.fragment;
+};
+
+ContactManager.on("initialize:after", function () {
+  if (Backbone.history) {
+    Backbone.history.start();
+  }
+
+  if (Backbone.history.fragment === "") {
+    Backbone.history.navigate("contacts");
+    ContactManager.trigger("contacts:list");
+  }
+});
