19372a50ee1fcae06f6555427f42d22d47b8a361
Jake Bellacera <jakeb@eleveninc.com>
2014-06-18 13:17:07 -0700

    Validating the edit contact form

--- assets/js/apps/contacts/edit/edit_controller.js
+++ assets/js/apps/contacts/edit/edit_controller.js
@@ -11,6 +11,14 @@ ContactManager.module("ContactsApp.Edit", function (Edit, ContactManager, Backbo
           view = new Edit.Contact({
             model: contact
           });
+
+          view.on("form:submit", function (data) {
+            if (contact.save(data)) {
+              ContactManager.trigger("contact:show", contact.get("id"));
+            } else {
+              view.triggerMethod("form:data:invalid", contact.validationError);
+            }
+          });
         } else {
           view = new ContactManager.ContactsApp.Show.MissingContact();
         }
