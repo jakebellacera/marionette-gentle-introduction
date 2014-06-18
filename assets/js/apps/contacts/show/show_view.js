656685c47f5ead15e521fa036d2d486faf7343e0
Jake Bellacera <jakeb@eleveninc.com>
2014-06-17 19:05:10 -0700

    Handling missing contacts

--- assets/js/apps/contacts/show/show_view.js
+++ assets/js/apps/contacts/show/show_view.js
@@ -2,4 +2,8 @@ ContactManager.module("ContactsApp.Show", function (Show, ContactManager, Backbo
   Show.Contact = Marionette.ItemView.extend({
     template: "#contact-view"
   });
+
+  Show.MissingContact = Marionette.ItemView.extend({
+    template: "#missing-contact-view"
+  });
 });
