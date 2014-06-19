14b8a88a8530469792a69d910e4f4d961d1285ab
Jake Bellacera <jakeb@eleveninc.com>
2014-06-18 18:35:52 -0700

    Adding a list layout and regions

--- assets/js/apps/contacts/list/list_controller.js
+++ assets/js/apps/contacts/list/list_controller.js
@@ -5,9 +5,18 @@ ContactManager.module("ContactsApp.List", function (List, ContactManager, Backbo
       ContactManager.mainRegion.show(loadingView);
 
       var fetchingContacts = ContactManager.request("contact:entities");
+
+      var contactsListLayout = new List.Layout();
+      var contactsListPanel = new List.Panel();
+
       $.when(fetchingContacts).done(function (contacts) {
         var contactsListView = new ContactManager.ContactsApp.List.Contacts({
           collection: contacts
+        });
+
+        contactsListLayout.on("show", function () {
+          contactsListLayout.panelRegion.show(contactsListPanel);
+          contactsListLayout.contactsRegion.show(contactsListView);
         });
 
         contactsListView.on("itemview:contact:show", function (childView, model) {
@@ -38,7 +47,7 @@ ContactManager.module("ContactsApp.List", function (List, ContactManager, Backbo
           model.destroy();
         });
 
-        ContactManager.mainRegion.show(contactsListView);
+        ContactManager.mainRegion.show(contactsListLayout);
       });
     }
   }
